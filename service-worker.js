1001d8c362b4b5e3e16a7802f0255d74192a511c
yspreen <yannick.spreen@gmail.com>
2021-08-26 19:46:38 +0000

    Add thumb.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e6bccca880924b88fb6db11a56b96983.js"
+  "/precache-manifest.9db240122ada822af6a32a5ecba5b2cb.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "fruits-web"});
